af103c89bcd2bea3a94efd9a69612f270271210e
jpham8 <newbhope8@gmail.com>
2019-10-06 22:09:44 -0700

    Update for play store and fix search
    After failing like more than 5 times uploading to the google play store, found out the API for stop auto completion changed. woho

--- app/src/main/java/com/friedrice/cubus/provider/SuggestionsProvider.java
+++ app/src/main/java/com/friedrice/cubus/provider/SuggestionsProvider.java
@@ -8,11 +8,13 @@ import android.database.MatrixCursor;
 import android.net.Uri;
 import android.provider.BaseColumns;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.friedrice.cubus.R;
@@ -27,7 +29,7 @@ import org.json.JSONObject;
 public class SuggestionsProvider extends ContentProvider {
 
     private static final String TAG = "SuggestionsProvider";
-    String [][] result;
+    String [][] suggestions;
 
     @Override
     public boolean onCreate() {
@@ -41,37 +43,34 @@ public class SuggestionsProvider extends ContentProvider {
         String query = selectionArgs[0];
 
         if(query.length() > 0) { //text present in SearchView
-            final String base_url = "https://developer.cumtd.com/api/v2.2/json/GetStopsBySearch";
-            final String key_param = "key";
+            final String base_url = "https://search.mtd.org/v1.0.0/stop/suggest/";
             final String query_param = "query";
 
             Uri urlUri = Uri.parse(base_url)
                     .buildUpon()
-                    .appendQueryParameter(key_param, getContext().getResources().getString(R.string.api_key))
-                    .appendQueryParameter(query_param, query)
+                    .appendPath(query)
                     .build();
             String url = urlUri.toString();
 
             RequestQueue queue = Volley.newRequestQueue(getContext());
-            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
                 @Override
-                public void onResponse(JSONObject response) {
-                    final String parent_param = "stops";
-                    final String id_param = "stop_id";
-                    final String name_param = "stop_name";
+                public void onResponse(JSONArray response) {
+                    final String result_param = "result";
+                    final String id_param = "id";
+                    final String name_param = "name";
 
                     try {
-                        JSONArray stopsArray = response.getJSONArray(parent_param); //json array from json object
-                        result = new String[stopsArray.length()][2]; //array to hold stop name and stop id
+                        suggestions = new String[response.length()][2]; //array to hold stop name and stop id
 
-                        for (int i = 0; i < stopsArray.length(); i++) {
-                            JSONObject stop = stopsArray.getJSONObject(i);
-                            String id = stop.getString(id_param);
-                            String name = stop.getString(name_param);
-                            result[i][0] = name;
-                            result[i][1] = id;
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject stop = response.getJSONObject(i);
+                            JSONObject result = stop.getJSONObject(result_param);
+                            String id = result.getString(id_param);
+                            String name = result.getString(name_param);
+                            suggestions[i][0] = name;
+                            suggestions[i][1] = id;
                         }
-
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -79,9 +78,10 @@ public class SuggestionsProvider extends ContentProvider {
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-
+                    Log.e("bruh", "mang");
                 }
             });
+
             queue.add(request);
 
             String[] columns = {
@@ -92,8 +92,8 @@ public class SuggestionsProvider extends ContentProvider {
 
             MatrixCursor cursor = new MatrixCursor(columns);
 
-            for(int i=0; i<result.length; i++) {
-                String[] suggestion = {Integer.toString(i), result[i][0], result[i][0], result[i][1]};
+            for (int i = 0; i < suggestions.length; i++) {
+                String[] suggestion = {Integer.toString(i), suggestions[i][0], suggestions[i][0], suggestions[i][1]};
                 cursor.addRow(suggestion);
             }
             return cursor;
